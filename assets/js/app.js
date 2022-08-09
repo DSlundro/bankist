43ff394817513f36a3818ace9f0511e1e01b8150
DSlundro <dragan.savic95@outlook.it>
2022-08-10 00:04:13 +0200

    calculate balance

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,40 +5,37 @@
 // BANKIST APP
 
 // Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [
+  {
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+  },
+  {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+  },
+  {
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+  },
+  {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+  },
+];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
+let labelBalance = document.querySelector('.balance__value');
 const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
@@ -91,11 +88,21 @@ const displayMovements = movements =>{
     containerMovements.insertAdjacentHTML('afterbegin', html)
   });
 };
-displayMovements(account1.movements)
+displayMovements(accounts[0].movements)
+
+/* CALCULATE BALANCE */
+const calcDisplayBalance = () => {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0)
+  return labelBalance.textContent = `${balance}€`
+
+}
+calcDisplayBalance(accounts[0].movements)
 
 /* CREATE USERNAME */
 const createUsernames = accs => accs.forEach(acc => acc.username = acc.owner.toLowerCase().split(' ').map(e => e[0]).join(''))
 createUsernames(accounts)
 
+const max = movements.reduce((acc, mov) => acc > mov ? acc : mov, movements[0])
+console.log(max);
 
 
